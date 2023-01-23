3c578540dfe85d0a2b963ec35be4e80398469c00
Michael Wu <mw10013@gmail.com>
2023-01-23 12:31:24 -0500

    cleanup

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,10 +1,12 @@
-import {Text} from "@/components/PolymorphicComponent"
+import { PolymorphicText } from "@/components/PolymorphicComponent";
 
 export default function Index() {
   // const { session } = useOutletContext<ContextType>();
   return (
     <div className="mt-8 max-w-xs mx-auto font-bold text-lg">
-      <Text as="div" color="indigo" className="text-4xl">Remix Template</Text>
+      <PolymorphicText as="div" color="indigo" className="text-4xl">
+        Remix Template
+      </PolymorphicText>
     </div>
   );
 }
