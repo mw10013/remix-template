e2f62385967808e10e25a2d0b5487b1c8779b32b
Michael Wu <mw10013@gmail.com>
2023-01-23 13:17:16 -0500

    add variant and color

--- app/components/PolymorphicComponent.tsx
+++ app/components/PolymorphicComponent.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import clsx from "clsx";
 
 type AsProp<C extends React.ElementType> = {
   as?: C;
@@ -50,8 +51,30 @@ export const PolymorphicText: PolymorphicTextComponent = React.forwardRef(
   }
 );
 
+const baseStyles = {
+  solid:
+    "inline-flex justify-center rounded-lg py-2 px-3 text-sm font-semibold outline-2 outline-offset-2 transition-colors",
+  outline:
+    "inline-flex justify-center rounded-lg border py-[calc(theme(spacing.2)-1px)] px-[calc(theme(spacing.3)-1px)] text-sm outline-2 outline-offset-2 transition-colors",
+};
+
+const variantStyles = {
+  solid: {
+    cyan: "relative overflow-hidden bg-cyan-500 text-white before:absolute before:inset-0 active:before:bg-transparent hover:before:bg-white/10 active:bg-cyan-600 active:text-white/80 before:transition-colors",
+    white:
+      "bg-white text-cyan-900 hover:bg-white/90 active:bg-white/90 active:text-cyan-900/70",
+    gray: "bg-gray-800 text-white hover:bg-gray-900 active:bg-gray-800 active:text-white/80",
+  },
+  outline: {
+    gray: "border-gray-300 text-gray-700 hover:border-gray-400 active:bg-gray-100 active:text-gray-700/80",
+  },
+};
+
 type PolymorphicButtonProps<C extends React.ElementType> =
-  PolymorphicComponentPropsWithRef<C, { variant?: string; color?: string }>;
+  PolymorphicComponentPropsWithRef<
+    C,
+    { variant?: keyof typeof variantStyles; color?: string }
+  >;
 
 type PolymorphicButtonComponent = <C extends React.ElementType = "button">(
   props: PolymorphicButtonProps<C>
@@ -63,8 +86,17 @@ export const PolymorphicButton: PolymorphicButtonComponent = React.forwardRef(
     ref?: PolymorphicRef<C>
   ) {
     const Component = as || "button";
-    return (
-        <Component ref={ref} className={className} {...props} />
-    )
+    variant = variant ?? "solid";
+    color = color ?? "gray";
+
+    const classNameString = clsx(
+      baseStyles[variant],
+      // @ts-ignore TODO: fix types
+      variantStyles[variant][color],
+      className
+    );
+
+    // return <Component ref={ref} className={className} {...props} />;
+    return <Component ref={ref} className={classNameString} {...props} />;
   }
 );
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,14 +1,30 @@
-import { PolymorphicButton, PolymorphicText } from "@/components/PolymorphicComponent";
+import {
+  PolymorphicButton,
+  PolymorphicText,
+} from "@/components/PolymorphicComponent";
+import { Link } from "@remix-run/react";
 
 export default function Index() {
   // const { session } = useOutletContext<ContextType>();
   return (
-    <div className="mt-8 max-w-xs mx-auto font-bold text-lg">
+    <div className="mt-8 max-w-sm mx-auto font-bold text-lg">
       <PolymorphicText as="div" color="indigo" className="text-4xl">
         Remix Template
       </PolymorphicText>
       <div className="flex gap-4 mt-8">
-        <PolymorphicButton>Button</PolymorphicButton>
+        <PolymorphicButton>Default</PolymorphicButton>
+        <PolymorphicButton as="button" color="cyan" onClick={() => alert("click")}>
+          Button
+        </PolymorphicButton>
+        <PolymorphicButton as={Link} color="cyan" to="#link">
+          Link
+        </PolymorphicButton>
+        <PolymorphicButton as="button" variant="outline" color="cyan" onClick={() => alert("click")}>
+          Button
+        </PolymorphicButton>
+        <PolymorphicButton as={Link} variant="outline" color="cyan" to="#link">
+          Link
+        </PolymorphicButton>
       </div>
     </div>
   );
