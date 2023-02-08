2a5cf13ce088c838460a927ce8103b6695ce9b0c
Michael Wu <mw10013@gmail.com>
2023-02-08 17:12:32 -0500

    wip

--- app/components/PolymorphicComponent1.tsx
+++ app/components/PolymorphicComponent1.tsx
@@ -1,22 +1,23 @@
-import React from 'react'
+import clsx from "clsx";
+import React from "react";
 
 // https://www.benmvp.com/blog/forwarding-refs-polymorphic-react-component-typescript/
 
-type Color = 'red' | 'green' | 'blue';
+type Color = "red" | "green" | "blue";
 
 // Source: https://github.com/emotion-js/emotion/blob/master/packages/styled-base/types/helper.d.ts
 // A more precise version of just React.ComponentPropsWithoutRef on its own
 export type PropsOf<
   C extends keyof JSX.IntrinsicElements | React.JSXElementConstructor<any>
-> = JSX.LibraryManagedAttributes<C, React.ComponentPropsWithoutRef<C>>
+> = JSX.LibraryManagedAttributes<C, React.ComponentPropsWithoutRef<C>>;
 
 type AsProp<C extends React.ElementType> = {
   /**
    * An override of the default HTML tag.
    * Can also be another React component.
    */
-  as?: C
-}
+  as?: C;
+};
 
 /**
  * Allows for extending a set of props (`ExtendedProps`) by an overriding set of props
@@ -26,7 +27,7 @@ type AsProp<C extends React.ElementType> = {
 export type ExtendableProps<
   ExtendedProps = {},
   OverrideProps = {}
-> = OverrideProps & Omit<ExtendedProps, keyof OverrideProps>
+> = OverrideProps & Omit<ExtendedProps, keyof OverrideProps>;
 
 /**
  * Allows for inheriting the props from the specified element type so that
@@ -36,7 +37,7 @@ export type ExtendableProps<
 export type InheritableElementProps<
   C extends React.ElementType,
   Props = {}
-> = ExtendableProps<PropsOf<C>, Props>
+> = ExtendableProps<PropsOf<C>, Props>;
 
 /**
  * A more sophisticated version of `InheritableElementProps` where
@@ -45,15 +46,13 @@ export type InheritableElementProps<
 export type PolymorphicComponentProps<
   C extends React.ElementType,
   Props = {}
-> = InheritableElementProps<C, Props & AsProp<C>>
+> = InheritableElementProps<C, Props & AsProp<C>>;
 
 /**
  * Utility type to extract the `ref` prop from a polymorphic component
  */
-export type PolymorphicRef<
-  C extends React.ElementType
->
- = React.ComponentPropsWithRef<C>['ref']
+export type PolymorphicRef<C extends React.ElementType> =
+  React.ComponentPropsWithRef<C>["ref"];
 /**
  * A wrapper of `PolymorphicComponentProps` that also includes the `ref`
  * prop for the polymorphic component
@@ -61,46 +60,100 @@ export type PolymorphicRef<
 export type PolymorphicComponentPropsWithRef<
   C extends React.ElementType,
   Props = {}
->
- = PolymorphicComponentProps<C, Props> & { ref?: PolymorphicRef<C> }
+> = PolymorphicComponentProps<C, Props> & { ref?: PolymorphicRef<C> };
 
 // 👇🏾👇🏾 sample usage in `Text` component 👇🏾👇🏾
 
 interface Props {
-  children: React.ReactNode
-  color?: Color
-  font?: 'thin' | 'regular' | 'heavy'
-  size?: '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10'
+  children: React.ReactNode;
+  color?: Color;
+  font?: "thin" | "regular" | "heavy";
+  size?: "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "10";
 }
 
 type TextProps<C extends React.ElementType> = PolymorphicComponentPropsWithRef<
   C,
   Props
->
+>;
 
-type TextComponent = <C extends React.ElementType = 'span'>(
-  props: TextProps<C>,
-) => React.ReactElement | null
+type TextComponent = <C extends React.ElementType = "span">(
+  props: TextProps<C>
+) => React.ReactElement | null;
 
 export const Text: TextComponent = React.forwardRef(
-  <C extends React.ElementType = 'span'>(
+  function PolymorphicTextComponent<C extends React.ElementType = "span">(
     {
       as,
       children,
-      font = 'regular',
-      size = '4',
-      color = 'blue',
+      font = "regular",
+      size = "4",
+      color = "blue",
       ...other
     }: TextProps<C>,
-    ref?: PolymorphicRef<C>,
-  ) => {
-    const classes = `${color} ${font} ${size}`
-    const Component = as || 'span'
+    ref?: PolymorphicRef<C>
+  ) {
+    const classes = `${color} ${font} ${size}`;
+    const Component = as || "span";
 
     return (
       <Component {...other} className={classes} ref={ref}>
         {children}
       </Component>
-    )
+    );
+  }
+);
+
+const baseStyles = {
+  solid:
+    "inline-flex justify-center rounded-lg py-2 px-3 text-sm font-semibold outline-2 outline-offset-2 transition-colors",
+  outline:
+    "inline-flex justify-center rounded-lg border py-[calc(theme(spacing.2)-1px)] px-[calc(theme(spacing.3)-1px)] text-sm outline-2 outline-offset-2 transition-colors",
+};
+
+const variantStyles = {
+  solid: {
+    cyan: "relative overflow-hidden bg-cyan-500 text-white before:absolute before:inset-0 active:before:bg-transparent hover:before:bg-white/10 active:bg-cyan-600 active:text-white/80 before:transition-colors",
+    white:
+      "bg-white text-cyan-900 hover:bg-white/90 active:bg-white/90 active:text-cyan-900/70",
+    gray: "bg-gray-800 text-white hover:bg-gray-900 active:bg-gray-800 active:text-white/80",
   },
-)
+  outline: {
+    gray: "border-gray-300 text-gray-700 hover:border-gray-400 active:bg-gray-100 active:text-gray-700/80",
+  },
+};
+
+interface ButtonBaseProps {
+  children: React.ReactNode;
+  variant?: keyof typeof variantStyles;
+  color?: keyof (typeof variantStyles)["solid"];
+  className?: string;
+}
+
+type ButtonProps<C extends React.ElementType> =
+  PolymorphicComponentPropsWithRef<C, ButtonBaseProps>;
+
+type ButtonComponent = <C extends React.ElementType = "button">(
+  props: ButtonProps<C>
+) => React.ReactElement | null;
+
+export const Button: ButtonComponent = React.forwardRef(
+  function PolymorphicButtonComponent<C extends React.ElementType = "button">(
+    {
+      as,
+      children,
+      variant = "solid",
+      color = "cyan",
+      className,
+      ...props
+    }: ButtonProps<C>,
+    ref?: PolymorphicRef<C>
+  ) {
+    const Component = as || "button";
+    className = clsx(baseStyles[variant], variantStyles[variant][color]);
+    return (
+      <Component ref={ref} className={className} {...props}>
+        {children}
+      </Component>
+    );
+  }
+);
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,6 +2,10 @@ import {
   PolymorphicButton,
   PolymorphicText,
 } from "@/components/PolymorphicComponent";
+import {
+  Text as PolymorphicText1,
+  Button as PolymorphicButton1,
+} from "@/components/PolymorphicComponent1";
 import { Link } from "@remix-run/react";
 
 export default function Index() {
@@ -12,23 +16,47 @@ export default function Index() {
         Remix Template
       </PolymorphicText>
       <div className="flex flex-col w-32 mx-auto gap-4 mt-8">
+        <PolymorphicText1 color="green" font="heavy" size="8">
+          PolymorphicText1
+        </PolymorphicText1>
+        <PolymorphicButton1>Default1</PolymorphicButton1>
         <PolymorphicButton>Default</PolymorphicButton>
-        <PolymorphicButton as="button" color="cyan" onClick={() => alert("click")}>
+        <PolymorphicButton
+          as="button"
+          color="cyan"
+          onClick={() => alert("click")}
+        >
           Button
         </PolymorphicButton>
         <PolymorphicButton as={Link} color="cyan" to="#link">
           Link
         </PolymorphicButton>
-        <PolymorphicButton as="button" variant="outline"onClick={() => alert("click")}>
+        <PolymorphicButton
+          as="button"
+          variant="outline"
+          onClick={() => alert("click")}
+        >
           Button
         </PolymorphicButton>
-        <PolymorphicButton as={Link} variant="outline"to="#link">
+        <PolymorphicButton as={Link} variant="outline" to="#link">
           Link
         </PolymorphicButton>
-        <PolymorphicButton as="button" variant="outline" color="gray" className="py-8" onClick={() => alert("click")}>
+        <PolymorphicButton
+          as="button"
+          variant="outline"
+          color="gray"
+          className="py-8"
+          onClick={() => alert("click")}
+        >
           Button
         </PolymorphicButton>
-        <PolymorphicButton as={Link} variant="outline" color="gray" className="py-8"  to="#link">
+        <PolymorphicButton
+          as={Link}
+          variant="outline"
+          color="gray"
+          className="py-8"
+          to="#link"
+        >
           Link
         </PolymorphicButton>
       </div>
